af94e2c86ee6931c7339818bb74969d329732f07
Ayenew Y. Demeke <ayennew@gmail.com>
2025-01-20 19:04:19 -0600

    Minor correction on routes

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,8 @@ Route::post('/fused-measurements', [MeasurementController::class, 'store']);
 
 // Get
 Route::get('/devices', [DeviceController::class, 'index']);
-Route::get('/readings', [ReadingController::class, 'index']);
-Route::get('/readings/{device_id}', [ReadingController::class, 'showByDevice']);
+Route::get('/device-readings', [ReadingController::class, 'index']);
+Route::get('/device-readings/{device_id}', [ReadingController::class, 'showByDevice']);
 Route::get('/fused-measurements', [MeasurementController::class, 'index']);
 Route::get('/fused-measurements/{device_id}', [MeasurementController::class, 'showByDevice']);
 
